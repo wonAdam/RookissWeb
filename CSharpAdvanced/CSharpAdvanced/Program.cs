f3d0e41d61163104c0ab589c49b7eebf2f89ae56
원동현 <ehdgus5500@gmail.com>
2022-01-01 19:46:09 +0900

    Async, Await 실습

---
+++ CSharpAdvanced/CSharpAdvanced/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced
+{
+    class Program
+    {
+        static Task Test()
+        {
+            Console.WriteLine("Start Task");
+            Task t = Task.Delay(3000);
+            return t;
+        }
+
+        static async Task<int> TestAsync()
+        {
+            Console.WriteLine("Start Task");
+            Task t = Task.Delay(3000);
+
+            await t;
+            Console.WriteLine("End TaskAsync");
+
+            return 1;
+        }
+
+        static async Task Main(string[] args)
+        {
+            Task<int> t = TestAsync();
+            int result = await t;
+
+            Console.WriteLine("While Start");
+            while (true)
+                ;
+        }
+    }
+}
